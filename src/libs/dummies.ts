c0b7ed18e187d9835b23d390de18e559578339d0
Kiju2 <thdydehfdl12@gmail.com>
2024-02-02 01:09:59 +0900

    feat: Add dummies for posts

---
+++ src/libs/dummies.ts
@@ -0,0 +1,87 @@
+type PostSchema = {
+  id: number;
+  title: string;
+  body: string;
+  image: string;
+};
+
+export const Posts: PostSchema[] = [
+  {
+    id: 1,
+    title: "Post 1",
+    body: "Post 1 body",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 2,
+    title: "Post 2",
+    body: "Post 2 body",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 3,
+    title: "Post 3",
+    body: "Post 3 body",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 4,
+    title: "Post 4",
+    body: "Post 4 body",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 5,
+    title: "Post 5",
+    body: "Post 5 body",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 6,
+    title: "Post 6",
+    body: "Post 6 body",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 7,
+    title: "Post 7",
+    body: "Post 7 body",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 8,
+    title: "Post 8",
+    body: "Post 8 body",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 9,
+    title: "Post 9",
+    body: "Post 9 body",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 10,
+    title: "Post 10",
+    body: "Post 10 body",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 11,
+    title: "Post 11",
+    body: "Post 11 body",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 12,
+    title: "Post 12",
+    body: "Post 12 body",
+    image: "https://picsum.photos/200/300",
+  },
+  {
+    id: 13,
+    title: "Post 13",
+    body: "Post 13 body",
+    image: "https://picsum.photos/200/300",
+  },
+];
