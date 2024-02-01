1ceb6aedd3a8902776149846cfcfe27b43421cd6
Kiju2 <thdydehfdl12@gmail.com>
2024-02-02 02:05:51 +0900

    feat: Add link for card

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -8,16 +8,6 @@ export default function PostsPage({}) {
       <div className="grid grid-cols-3 gap-4">
         {Posts.map((post) => (
           <Card key={post.id} {...post} />
-          // <div className="flex flex-col" key={post.id}>
-          //   <Image
-          //     src={post.image}
-          //     alt={post.title}
-          //     width={300}
-          //     height={300}
-          //     className="rounded-lg"
-          //   />
-          //   <h2 className="text-2xl font-bold">{post.title}</h2>
-          // </div>
         ))}
       </div>
     </main>
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,17 +1,20 @@
 import { PostSchema } from "@/models/post.model";
 import Image from "next/image";
+import Link from "next/link";
 
-export default function Card({ title, image }: PostSchema) {
+export default function Card({ title, image, id }: PostSchema) {
   return (
-    <div className="flex flex-col">
-      <Image
-        src={image}
-        alt={title}
-        width={300}
-        height={300}
-        className="rounded-lg"
-      />
-      <h2 className="text-2xl font-bold">{title}</h2>
-    </div>
+    <Link href={`/posts/${id}`}>
+      <div className="flex flex-col justify-center items-center p-4 border border-gray-300 rounded-lg cursor-pointer hover:shadow-lg transition-shadow">
+        <Image
+          src={image}
+          alt={title}
+          width={300}
+          height={300}
+          className="rounded-lg"
+        />
+        <h2 className="text-2xl font-bold">{title}</h2>
+      </div>
+    </Link>
   );
 }
