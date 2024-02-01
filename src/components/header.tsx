7a41fd9b16446218b7c9557e826afbe906d579a1
Kiju2 <thdydehfdl12@gmail.com>
2024-02-02 01:03:34 +0900

    init

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="flex justify-between items-center p-4">
+      <h1 className="font-bold text-2xl">Next.js</h1>
+      <nav className="flex">
+        <Link href="/" className="mr-4">
+          Home
+        </Link>
+        <Link href="/about">About</Link>
+      </nav>
+    </header>
+  );
+}
