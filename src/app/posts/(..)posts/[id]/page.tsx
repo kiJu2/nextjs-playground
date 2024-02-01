c9e04cb4a146cdb10496f68030fa87a12408e19c
Kiju2 <thdydehfdl12@gmail.com>
2024-02-02 02:49:03 +0900

    feat: -

--- src/app/posts/(..)posts/[id]/page.tsx
+++ src/app/posts/(..)posts/[id]/page.tsx
@@ -1,7 +1,32 @@
-export default function PostsInterceptIdPage() {
+import Modal from "@/components/modal";
+import findPostById from "@/servies/post.service";
+import Image from "next/image";
+
+export default function PostsInterceptIdPage({
+  params: { id },
+}: {
+  params: {
+    id: string;
+  };
+}) {
+  const post = findPostById(+id);
+
+  // TODO: Add ErrorBoundary
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
   return (
-    <main className="">
-      <h1 className="">Hello World</h1>
-    </main>
+    <Modal>
+      <h1 className="font-semibold text-2xl mb-2">{post.title}</h1>
+      <p className="text-lg mb-4">{post.body}</p>
+      <Image
+        src={post.image}
+        alt={post.title}
+        width={300}
+        height={300}
+        className="rounded-lg"
+      />
+    </Modal>
   );
 }
